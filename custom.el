06bca2ea8a034a0758f293be238d6976ccfeed63
joafr <joakim@frogren.se>
2021-03-12 16:56:47 +0100

    Försöker få ivy-bibtex att laddas vid emacs-start

--- custom.el
+++ custom.el
@@ -1,11 +1,11 @@
-(custom-set-variables
+ ;; (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ivy-bibtex poly-R pdf-tools pandoc-mode pandoc lua-mode exec-path-from-shell ewal-doom-themes ess)))
-(custom-set-faces
+ ;; '(package-selected-packages
+ ;;  '(ivy-bibtex poly-R pdf-tools pandoc-mode pandoc lua-mode exec-path-from-shell ewal-doom-themes ess)))
+ ;; (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
