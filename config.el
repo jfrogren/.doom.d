9fc98f0e2b484d237632428a6e7ac285c99ac581
joafr <joakim@frogren.se>
2021-04-20 06:26:55 +0200

    Första fungerande versionen där org-noter funkar!!

--- config.el
+++ config.el
@@ -158,7 +158,7 @@
 ;;------------------------------------------------------------------------------------
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+;; (setq org-directory "~/org/zettelkasten")
 
 ;;------------------------------------------------------------------------------------
 ;; pdf-tools
@@ -171,12 +171,11 @@
   (setq-default pdf-view-display-size 'fit-page)
   (setq pdf-annot-activate-created-annotations t)
   (setq pdf-view-resize-factor 1.1)
-  (setq pdf-view-use-unicode-ligther nil)
-  )
+  (setq pdf-view-use-unicode-ligther nil))
 
-;;---------------------------------------------------------------------------------------
-;;  ivy-bibtex
-;;---------------------------------------------------------------------------------------
+(setq deft-directory "/Users/joafr/org/zettelkasten/"
+      deft-use-filename-as-title nil)
+
 (autoload 'ivy-bibtex "ivy-bibtex" "" t)
 ;; ivy-bibtex requires ivy's `ivy--regex-ignore-order` regex builder, which
 ;; ignores the order of regexp tokens when searching for matching candidates.
@@ -186,110 +185,135 @@
         (t . ivy--regex-plus)))
 (setq bibtex-completion-bibliography "/Users/joafr/Documents/bibfiles/library.bib"
       bibtex-completion-library-path "/Users/joafr/Documents/bibpdfs"
-      bibtex-completion-notes-path "/Users/joafr/org/zettelkasten/bibnotes"
-      bibtex-completion-notes-extension ".org"
-      bibtex-completion-find-additional-pdfs t
-      bibtex-completion-notes-template-multiple-files "Notes on ${author-or-editor} (${year}) - ${title}"
-      bibtex-completion-notes-template-one-file "# Notes on ${author-or-editor} (${year}): ${title}")
-(delete-file "~/Library/Colors/Emacs.clr")
+      bibtex-completion-notes-path "/Users/joafr/org/zettelkasten/"
+      bibtex-completion-notes-template-multiple-files "")
 
-;;------------------------------------------------------------------------------------
-;;  org-ref
-;;------------------------------------------------------------------------------------
-(setq joafr/bibliography-path "/Users/joafr/Documents/bibfiles/library.bib")
-(setq joafr/pdf-path  "/Users/joafr/Documents/bibpdfs/")
-(setq joafr/bibliography-notes "/Users/joafr/org/zettelkasten/bibnotes")
+(setq org-directory "/Users/joafr/org/zettelkasten/"
+      org-roam-directory "/Users/joafr/org/zettelkasten/")
 
-(use-package! org-ref
-  :after org
-  :commands
-  (org-ref-cite-hydra/body
-   org-ref-bibtex-hydra/body)
- ;; :init
- ; code to run before loading org-ref
-  :config
-  (setq
-   org-ref-completion-library 'org-ref-ivy-cite
-   org-ref-get-pdf-filename-function 'org-ref-get-pdf-filename-helm-bibtex
-   org-ref-default-bibliography (list "/Users/joafr/Documents/bibfiles/library.bib")
-   org-ref-bibliography-notes "/Users/joafr/org/zettelkasten/bibnotes.org"
-   org-ref-note-title-format "#+TITLE: ${citekey}: ${title}\n#+ROAM_KEY: ${ref}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n"
-   org-ref-notes-directory joafr/bibliography-notes
-   org-ref-notes-function 'orb-edit-notes
+(use-package org-ref
+    :after org
+    :commands
+    (org-ref-cite-hydra/body
+     org-ref-bibtex-hydra/body)
+    :init
+    (let ((cache-dir (concat doom-cache-dir "org-ref")))
+    (unless (file-exists-p cache-dir)
+      (make-directory cache-dir t))
+    (setq orhc-bibtex-cache-file (concat cache-dir "/orhc-bibtex-cache")))
+    :config
+    (setq
+         org-ref-completion-library 'org-ref-ivy-cite
+         org-ref-get-pdf-filename-function 'org-ref-get-pdf-filename-helm-bibtex
+         org-ref-default-bibliography (list "/Users/joafr/Documents/bibfiles/library.bib")
+         org-ref-bibliography-notes "/Users/joafr/org/zettelkasten/"
+         org-ref-note-title-format "* TODO %y - %t\n :PROPERTIES:\n  :Custom_ID: %k\n  :NOTER_DOCUMENT: %F\n :ROAM_KEY: cite:%k\n  :AUTHOR: %9a\n  :JOURNAL: %j\n  :YEAR: %y\n  :VOLUME: %v\n  :PAGES: %p\n  :DOI: %D\n  :URL: %U\n :END:\n\n"
+         org-ref-notes-directory "/Users/joafr/org/zettelkasten/"
+         org-ref-notes-function 'orb-edit-notes
     ))
 
-(after! org-ref
-  (setq
-   bibtex-completion-notes-path joafr/bibliography-notes
-   bibtex-completion-bibliography "/Users/joafr/Documents/bibfiles/library.bib"
-   bibtex-completion-pdf-field "file"
-   bibtex-completion-notes-template-multiple-files
-   (concat
-    "#+TITLE: ${title}\n"
-    "#+ROAM_KEY: cite:${=key=}"
-    "#+ROAM_TAGS: ${keywords}"
-    "#+CREATED:%<%Y-%m-%d-%H-%M-%S>"
-    "Time-stamp: <>\n"
-    "- tags :: \n"
-    "* NOTES \n"
-    ":PROPERTIES:\n"
-    ":Custom_ID: ${=key=}\n"
-    ":NOTER_DOCUMENT: %{file}\n"
-    ":AUTHOR: ${author-abbrev}\n"
-    ":JOURNAL: ${journaltitle}\n"
-    ":DATE: ${date}\n"
-    ":YEAR: ${year}\n"
-    ":DOI: ${doi}\n"
-    ":URL: ${url}\n"
-    ":END:\n\n"
-    )
-   )
+(use-package org-roam
+  :hook (org-load . org-roam-mode)
+  :commands (org-roam-buffer-toggle-display
+             org-roam-find-file
+             org-roam-graph
+             org-roam-insert
+             org-roam-switch-to-buffer
+             org-roam-dailies-date
+             org-roam-dailies-today
+             org-roam-dailies-tomorrow
+             org-roam-dailies-yesterday)
+  :preface
+  ;; Set this to nil so we can later detect whether the user has set a custom
+  ;; directory for it, and default to `org-directory' if they haven't.
+  (defvar org-roam-directory nil)
+  :init
+  :config
+  (setq org-roam-directory (expand-file-name (or org-roam-directory "roam")
+                                             org-directory)
+        org-roam-verbose nil  ; https://youtu.be/fn4jIlFwuLU
+        org-roam-buffer-no-delete-other-windows t ; make org-roam buffer sticky
+        org-roam-completion-system 'default
 )
 
-;;------------------------------------------------------------------------------------
-;; org-roam
-;;------------------------------------------------------------------------------------
+  ;; Normally, the org-roam buffer doesn't open until you explicitly call
+  ;; `org-roam'. If `+org-roam-open-buffer-on-find-file' is non-nil, the
+  ;; org-roam buffer will be opened for you when you use `org-roam-find-file'
+  ;; (but not `find-file', to limit the scope of this behavior).
+  (add-hook 'find-file-hook
+    (defun +org-roam-open-buffer-maybe-h ()
+      (and +org-roam-open-buffer-on-find-file
+           (memq 'org-roam-buffer--update-maybe post-command-hook)
+           (not (window-parameter nil 'window-side)) ; don't proc for popups
+           (not (eq 'visible (org-roam-buffer--visibility)))
+           (with-current-buffer (window-buffer)
+             (org-roam-buffer--get-create)))))
+
+  ;; Hide the mode line in the org-roam buffer, since it serves no purpose. This
+  ;; makes it easier to distinguish among other org buffers.
+  (add-hook 'org-roam-buffer-prepare-hook #'hide-mode-line-mode))
+
+
+;; Since the org module lazy loads org-protocol (waits until an org URL is
+;; detected), we can safely chain `org-roam-protocol' to it.
+(use-package org-roam-protocol
+  :after org-protocol)
+
+
+(use-package company-org-roam
+  :after org-roam
+  :config
+  (set-company-backend! 'org-mode '(company-org-roam company-yasnippet company-dabbrev)))
+
 (use-package! org-roam-bibtex
   :after org-roam
   :hook (org-roam-mode . org-roam-bibtex-mode)
   :config
-  (require 'org-ref)) ; optional: if Org Ref is not loaded anywhere else, load it here
-;;  :bind (:map org-mode-map
-;;         (("C-c n a" . orb-note-actions)))
+  (require 'org-ref))
 
-(setq org-roam-directory "/Users/joafr/org/zettelkasten")
-;;;; Add the cite-key in the title of the notes and make sure they end up in bibnotes folder.
-;;(setq orb-templates
-;;      '(("r" "ref" plain (function org-roam-capture--get-point) "%?"
-;;         :file-name "bibnotes/${citekey}"
-;;         :head "#+TITLE: ${citekey}: ${title}\n#+ROAM_KEY: ${ref}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n"
-;;         :unnarrowed t)))
+(setq org-roam-bibtex-preformat-keywords
+      '("citekey" "title" "url" "file" "author-or-editor" "keywords"))
 (setq orb-templates
-      '(("r" "ref" plain (function org-roam-capture--get-point) "%?"
-         :file-name "bibnotes/${citekey}"
-         :head "#+TITLE: ${citekey}: ${title}\n#+ROAM_KEY: ${ref}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n"
-         :unnarrowed t)))
+        '(("r" "ref" plain (function org-roam-capture--get-point)
+           ""
+           :file-name "${citekey}"
+           :head "#+TITLE: ${citekey}: ${title}\n#+ROAM_KEY: ${ref}
 
-(setq org-roam-capture-templates
-        '(("d" "default" plain (function org-roam-capture--get-point) ""
-         :file-name "%<%Y%m%d%H%M%S>-${slug}"
-         :head "#+TITLE: ${title}\n#+ROAM_ALIAS: \n#+CREATED: %U\n#+LAST_MODIFIED: %U\n"
-         :unnarrowed t)))
+- tags ::
+- keywords ::
 
-;;-----------------------------------------------------------------------------------------
-;;  org-noter
-;;------------------------------------------------------------------------------------------
-(use-package! org-noter
+\n* ${citekey}: ${title}\n  :PROPERTIES:\n  :Custom_ID: ${citekey}\n  :URL: \n  :AUTHOR: ${author}\n  :NOTER_DOCUMENT: ${file}\n  :NOTER_PAGE: \n  :END:\n\n"
+
+           :unnarrowed t)))
+
+(use-package org-noter
   :after org
   :config
   (setq
-   ;; The WM can handle splits
-   ;;org-noter-notes-window-location 'other-frame
+   ;; The WM can handle splits - var tidigare "other-frame"
+   org-noter-notes-window-location 'horizontal-split
    ;; Please stop opening frames
-   ;;org-noter-always-create-frame nil
+   org-noter-always-create-frame nil
    ;; I want to see the whole file
    org-noter-hide-other nil
-   ;; Everything is relative to the rclone mega
-   org-noter-notes-search-path joafr/bibliography-notes
+   ;; Everything is relative to the main notes file
+   org-noter-notes-search-path (list "/Users/joafr/org/zettelkasten/")
+)
    )
-  )
+
+;; Actually start using templates
+(after! org-capture
+  ;; Firefox and Chrome
+  (add-to-list 'org-capture-templates
+               '("P" "Protocol" entry ; key, name, type
+                 (file+headline +org-capture-notes-file "Inbox") ; target
+                 "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?"
+                 :prepend t ; properties
+                 :kill-buffer t))
+  (add-to-list 'org-capture-templates
+               '("L" "Protocol Link" entry
+                 (file+headline +org-capture-notes-file "Inbox")
+                 "* %? [[%:link][%(transform-square-brackets-to-round-ones \"%:description\")]]\n"
+                 :prepend t
+                 :kill-buffer t))
+)
