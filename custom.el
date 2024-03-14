a01c2df310d2af8d03ebbb952f6a115d0f30e657
joafr <joakim@frogren.se>
2024-03-14 09:36:29 +0100

    Nu funkar ivy-bibtex!

--- custom.el
+++ custom.el
@@ -5,15 +5,18 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages '(poly-R polymode pdf-tools ivy-bibtex doom-themes)))
-(custom-set-faces
+;; (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
-
+;; )
+(setq bibtex-completion-bibliography "/Users/joafr/Documents/bibfiles/library.bib"
+      )
+(setq bibtex-completion-library-path "/Users/joafr/Documents/bibpdfs"
+      )
 (setq bibtex-completion-notes-path "/Users/joafr/Documents/bibnotes"
-      bibtex-completion-notes-extension ".md"
-      bibtex-completion-find-additional-pdfs t
-      bibtex-completion-notes-template-multiple-files "# Anteckningar om ${author-or-editor} (${year}) - ${title}"
-)
+       bibtex-completion-notes-extension ".md"
+       bibtex-completion-find-additional-pdfs t
+       bibtex-completion-notes-template-multiple-files "# Anteckningar om ${author-or-editor} (${year}) - ${title}"
+      )
